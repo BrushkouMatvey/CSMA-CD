06d30b738fd6dfcef2a85628653b4df842e99f25
BrushkouMatvey <brushkoumatvey@gmail.com>
2019-11-03 17:02:13 +0300

    Add project files.

---
+++ lab4toks/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace lab4toks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Bunifu.ToggleSwitch.ToggleState toggleState7 = new Bunifu.ToggleSwitch.ToggleState();
+            Bunifu.ToggleSwitch.ToggleState toggleState8 = new Bunifu.ToggleSwitch.ToggleState();
+            Bunifu.ToggleSwitch.ToggleState toggleState9 = new Bunifu.ToggleSwitch.ToggleState();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.debugTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.switchPackageMode = new Bunifu.ToggleSwitch.BunifuToggleSwitch();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputTextBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.inputTextBox.Location = new System.Drawing.Point(6, 48);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(404, 130);
+            this.inputTextBox.TabIndex = 2;
+            this.inputTextBox.Click += new System.EventHandler(this.inputTextBox_Click);
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            this.inputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputTextBox_KeyPress);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(386, -1);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(31, 31);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 5;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.BunifuImageButton1_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.outputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputTextBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTextBox.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.outputTextBox.Location = new System.Drawing.Point(6, 212);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(404, 130);
+            this.outputTextBox.TabIndex = 6;
+            // 
+            // debugTextBox
+            // 
+            this.debugTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.debugTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.debugTextBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.debugTextBox.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.debugTextBox.Location = new System.Drawing.Point(6, 420);
+            this.debugTextBox.Multiline = true;
+            this.debugTextBox.Name = "debugTextBox";
+            this.debugTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.debugTextBox.Size = new System.Drawing.Size(404, 150);
+            this.debugTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            this.label2.Location = new System.Drawing.Point(3, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            this.label3.Location = new System.Drawing.Point(4, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Control and debug";
+            // 
+            // switchPackageMode
+            // 
+            this.switchPackageMode.Animation = 5;
+            this.switchPackageMode.BackColor = System.Drawing.Color.Transparent;
+            this.switchPackageMode.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("switchPackageMode.BackgroundImage")));
+            this.switchPackageMode.Checked = true;
+            this.switchPackageMode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.switchPackageMode.InnerCirclePadding = 3;
+            this.switchPackageMode.Location = new System.Drawing.Point(8, 389);
+            this.switchPackageMode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.switchPackageMode.Name = "switchPackageMode";
+            this.switchPackageMode.Size = new System.Drawing.Size(34, 21);
+            this.switchPackageMode.TabIndex = 15;
+            toggleState7.BackColor = System.Drawing.Color.Empty;
+            toggleState7.BackColorInner = System.Drawing.Color.Empty;
+            toggleState7.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(0)))), ((int)(((byte)(140)))));
+            toggleState7.BorderColorInner = System.Drawing.Color.Empty;
+            toggleState7.BorderRadius = 1;
+            toggleState7.BorderRadiusInner = 1;
+            toggleState7.BorderThickness = 1;
+            toggleState7.BorderThicknessInner = 1;
+            this.switchPackageMode.ToggleStateDisabled = toggleState7;
+            toggleState8.BackColor = System.Drawing.Color.Empty;
+            toggleState8.BackColorInner = System.Drawing.Color.Empty;
+            toggleState8.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(0)))), ((int)(((byte)(140)))));
+            toggleState8.BorderColorInner = System.Drawing.Color.Empty;
+            toggleState8.BorderRadius = 1;
+            toggleState8.BorderRadiusInner = 1;
+            toggleState8.BorderThickness = 1;
+            toggleState8.BorderThicknessInner = 1;
+            this.switchPackageMode.ToggleStateOff = toggleState8;
+            toggleState9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            toggleState9.BackColorInner = System.Drawing.Color.White;
+            toggleState9.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            toggleState9.BorderColorInner = System.Drawing.Color.White;
+            toggleState9.BorderRadius = 17;
+            toggleState9.BorderRadiusInner = 11;
+            toggleState9.BorderThickness = 1;
+            toggleState9.BorderThicknessInner = 1;
+            this.switchPackageMode.ToggleStateOn = toggleState9;
+            this.switchPackageMode.Value = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(161)))), ((int)(((byte)(137)))));
+            this.label5.Location = new System.Drawing.Point(49, 389);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Burstmode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(417, 582);
+            this.Controls.Add(this.switchPackageMode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.debugTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.inputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox debugTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private Bunifu.ToggleSwitch.BunifuToggleSwitch switchPackageMode;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ lab4toks/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace lab4toks
+{
+    public partial class Form1 : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        private String inputTextBoxStrToPackage;
+        private StringBuilder OldText = new StringBuilder();
+        const double TIME_SLOT = 0.0512;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inputTextBoxStrToPackage = string.Empty;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void BunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InputTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Regex.IsMatch(e.KeyChar.ToString(), "[A-Za-z0-9\b]"))
+                e.Handled = false;
+            else e.Handled = true;
+        }
+        
+        private bool CollisionAnalysis()
+        {
+            int timeMillisecond = DateTime.Now.Millisecond;
+            if (timeMillisecond % 2 == 0)
+                return true;
+            else return false;
+        }
+
+        private bool ListeningCarrierFrequency()
+        {
+            int timeMillisecond = DateTime.Now.Millisecond;
+            if (timeMillisecond % 2 != 0)
+                return true;
+            else return false;
+        }
+
+        private double GetDelay(int count)
+        {
+            Random randomNumber = new Random();
+            int rnd = randomNumber.Next(count, 10);
+            return TIME_SLOT * Convert.ToDouble(randomNumber.Next(0, Convert.ToInt32(Math.Pow(2, randomNumber.Next(count, 10)))));
+        }
+
+        private void InputTextBox_GotFocus(object sender, EventArgs e) => OldText = new StringBuilder(inputTextBox.Text);
+
+        private void allAnalysis()
+        {
+            int i = 0;
+            debugTextBox.AppendText(">>");
+            while (ListeningCarrierFrequency())
+            {
+                i++;
+                if (i > 10)
+                {
+                    debugTextBox.AppendText(Environment.NewLine);
+                    return;
+                }
+                debugTextBox.AppendText("X");
+            }
+            System.Threading.Thread.Sleep((int)GetDelay(i));//окно коллизий
+            while (CollisionAnalysis())
+            {
+                i++;
+                if (i > 10)
+                {
+                    debugTextBox.AppendText(Environment.NewLine);
+                    System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
+                    return;
+                }
+                debugTextBox.AppendText("x");
+                System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
+            }
+            debugTextBox.AppendText(Environment.NewLine);
+        }
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (switchPackageMode.Value == true)
+            {
+                if (OldText.Length <= inputTextBox.Text.Length)
+                {
+                    inputTextBoxStrToPackage += inputTextBox.Text.Substring(OldText.Length);
+                    if (inputTextBoxStrToPackage.Length == 4)
+                    {
+                        allAnalysis();
+                        outputTextBox.AppendText(inputTextBoxStrToPackage);                        
+                        inputTextBoxStrToPackage = string.Empty;
+                    }
+                }
+            }
+            else
+            {
+                allAnalysis();
+                outputTextBox.AppendText(inputTextBox.Text.Substring(inputTextBox.Text.Length - 1));
+            }
+            OldText.Length = inputTextBox.Text.Length;
+        }
+        private void inputTextBox_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.SelectionStart != inputTextBox.Text.Length)
+            {
+                inputTextBox.SelectionStart = inputTextBox.Text.Length;
+            }
+        }
+    }
+}
