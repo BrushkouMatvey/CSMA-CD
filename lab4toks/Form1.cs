21f9ec875c3cd20156a00774a4b742028141c352
BrushkouMatvey <brushkoumatvey@gmail.com>
2019-11-06 18:10:31 +0300

    Done

--- lab4toks/Form1.cs
+++ lab4toks/Form1.cs
@@ -11,8 +11,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-
-
 namespace lab4toks
 {
     public partial class Form1 : Form
@@ -22,7 +20,8 @@ namespace lab4toks
         public const int HT_CAPTION = 0x2;
         private String inputTextBoxStrToPackage;
         private StringBuilder OldText = new StringBuilder();
-        const double TIME_SLOT = 0.0512;
+        const double TIME_SLOT = /*0.0512*/3;
+        const int COLLISION_WINDOW_TIME = 2;
 
         [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
@@ -67,6 +66,13 @@ namespace lab4toks
             else return false;
         }
 
+        private bool LateCollisionAnalysis()
+        {
+            int timeMillisecond = DateTime.Now.Millisecond;
+            if (timeMillisecond % 10 == 0)
+                return true;
+            else return false;
+        }
         private bool ListeningCarrierFrequency()
         {
             int timeMillisecond = DateTime.Now.Millisecond;
@@ -82,58 +88,95 @@ namespace lab4toks
             return TIME_SLOT * Convert.ToDouble(randomNumber.Next(0, Convert.ToInt32(Math.Pow(2, randomNumber.Next(count, 10)))));
         }
 
+        private String send(String msg) { return msg; }
+
         private void InputTextBox_GotFocus(object sender, EventArgs e) => OldText = new StringBuilder(inputTextBox.Text);
 
-        private void allAnalysis()
+        private void collisionWindow()
         {
-            int i = 0;
-            debugTextBox.AppendText(">>");
-            while (ListeningCarrierFrequency())
-            {
-                i++;
-                if (i > 10)
-                {
-                    debugTextBox.AppendText(Environment.NewLine);
-                    return;
-                }
-                debugTextBox.AppendText("X");
-            }
-            System.Threading.Thread.Sleep((int)GetDelay(i));//окно коллизий
-            while (CollisionAnalysis())
-            {
-                i++;
-                if (i > 10)
-                {
-                    debugTextBox.AppendText(Environment.NewLine);
-                    System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
-                    return;
-                }
-                debugTextBox.AppendText("x");
-                System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
-            }
-            debugTextBox.AppendText(Environment.NewLine);
+            System.Threading.Thread.Sleep(COLLISION_WINDOW_TIME * 300);//окно коллизий
         }
         private void inputTextBox_TextChanged(object sender, EventArgs e)
         {
+            int i = 0;
             if (switchPackageMode.Value == true)
             {
                 if (OldText.Length <= inputTextBox.Text.Length)
                 {
                     inputTextBoxStrToPackage += inputTextBox.Text.Substring(OldText.Length);
+                    int j = 0;
                     if (inputTextBoxStrToPackage.Length == 4)
                     {
-                        allAnalysis();
-                        outputTextBox.AppendText(inputTextBoxStrToPackage);                        
+                        debugTextBox.AppendText(">>");
+                        if (j == 0)
+                        {
+                            do
+                            {
+                                String symbolSend = send(inputTextBox.Text.Substring(inputTextBox.Text.Length - 1));//отправка символа
+                                collisionWindow();
+                                if (CollisionAnalysis())
+                                {
+                                    i++;
+                                    if (i > 10)
+                                    {
+                                        i = 0;
+                                        debugTextBox.AppendText(Environment.NewLine);
+                                        return;
+                                    }
+                                    debugTextBox.AppendText("x");
+                                    System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
+                                }
+                                else break;
+                            } while (true);
+                            outputTextBox.AppendText(inputTextBoxStrToPackage[j].ToString());
+                            j++;
+                        }
+                        while(j < 4)
+                        {
+                            if(LateCollisionAnalysis())
+                            {
+                                debugTextBox.AppendText(Environment.NewLine);
+                                debugTextBox.AppendText(">>LateCollision");
+                                debugTextBox.AppendText(Environment.NewLine);
+                                inputTextBoxStrToPackage = string.Empty;
+                                OldText.Length = inputTextBox.Text.Length;
+                                return;
+                            }                                                      
+                            outputTextBox.AppendText(inputTextBoxStrToPackage[j].ToString());
+                            j++;
+                        }
                         inputTextBoxStrToPackage = string.Empty;
+                        debugTextBox.AppendText(Environment.NewLine);
                     }
                 }
             }
             else
             {
-                allAnalysis();
+                debugTextBox.AppendText(">>");
+                do
+                {
+                    while (ListeningCarrierFrequency()) { }//канал свободен или нет?
+                    String symbolSend = send(inputTextBox.Text.Substring(inputTextBox.Text.Length - 1));//отправка символа
+                    collisionWindow();
+                    if (CollisionAnalysis())
+                    {
+                        i++;
+                        if (i > 10)
+                        {
+                            debugTextBox.AppendText(Environment.NewLine);
+                            return;
+                        }
+                        debugTextBox.AppendText("x");
+                        System.Threading.Thread.Sleep((int)GetDelay(i));//вычисление случайной задержки
+                    }
+                    else break;
+                } while (true);
                 outputTextBox.AppendText(inputTextBox.Text.Substring(inputTextBox.Text.Length - 1));
+                debugTextBox.AppendText(Environment.NewLine);
             }
             OldText.Length = inputTextBox.Text.Length;
+
+            
         }
         private void inputTextBox_Click(object sender, EventArgs e)
         {
